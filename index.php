774ca1530ee4d8cf0de052d3ac2ef3f7a0daf0ea
POTCHONA Justin <potchjust@gmail.com>
2019-05-16 11:41:43 +0000

    Fichier index modifié

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 require_once '../config/Bootstrap.php';
-require_once '../routes/api.php';
+$app->post('/api/login', '\Api\Controller\LoginController:login');
+$app->post('/api/service/add', '\Api\Controller\ServicesController:insert');
+$app->get('/api/services/all', '\Api\Controller\ServicesController:getAllServices');
+$app->get('/api/service/show/{serviceId}', '\Api\Controller\ServicesController:show');
 $app->run();
