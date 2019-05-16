46bc216d7568840c0d4bd6815efd7adaf3a86418
POTCHONA Justin <potchjust@gmail.com>
2019-05-16 11:39:31 +0000

    Fichier index modifié

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@
 
 $app->post('/api/login', '\Api\Controller\LoginController:login');
 $app->post('/api/service/add', '\Api\Controller\ServicesController:insert');
-$app->get('/api/service/all', '\Api\Controller\ServicesController:getAllServices');
+$app->get('/api/services/all', '\Api\Controller\ServicesController:getAllServices');
 $app->get('/api/service/show/{serviceId}', '\Api\Controller\ServicesController:show');
 
 ?>
